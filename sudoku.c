defee14e676ec9cd05a2053e4313baa29601eb7e
Mark Gondree <gondree@gmail.com>
2020-03-04 15:20:02 -0800

    Added first draft of assignment

---
+++ sudoku.c
@@ -0,0 +1,56 @@
+#include <stdio.h>  
+#include <unistd.h> 
+#include <stdlib.h>
+#include <getopt.h>
+
+/* These are the only two global variables allowed in your program */
+static int verbose = 0;
+static int use_fork = 0;
+
+
+// This is a simple function to parse the --fork argument.
+// It also supports --verbose, -v
+void parse_args(int argc, char *argv[])
+{
+    int c;
+    while (1)
+    {
+        static struct option long_options[] =
+        {
+            {"verbose", no_argument,       0, 'v'},
+            {"fork",    no_argument,       0, 'f'},
+            {0, 0, 0, 0}
+        };
+        int option_index = 0;
+        c = getopt_long (argc, argv, "vf", long_options, &option_index);
+        if (c == -1) break;
+
+        switch (c)
+        {
+            case 'f':
+                use_fork = 1;
+                break;
+            case 'v':
+                verbose = 1;
+                break;
+            default:
+                exit(1);
+        }
+    }
+}
+
+
+int main(int argc, char *argv[])
+{
+    parse_args(argc, argv);
+
+    if (verbose && use_fork) {
+        printf("We are forking child processes as workers.\n");
+    } else if (verbose) {
+        printf("We are using worker threads.\n");
+    }
+
+
+    return 0;
+}
+
